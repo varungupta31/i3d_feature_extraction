5d57ccc6abc179c3862c7b71bbe39db445f3d7f0
Varun Gupta <varungupta@varunmac.local>
2023-08-21 22:30:19 +0530

    added pretrained flow and rgb weights

---
+++ cmd_dataloader.py
@@ -0,0 +1,18 @@
+import torch
+import torch.utils.data as data_utl
+from torch.utils.data.dataloader import default_collate
+
+import numpy as np
+import json
+import csv
+import h5py
+
+import os
+import os.path
+
+import cv2
+
+
+
+class CondensedMoviesClips():
+    pass
